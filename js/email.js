f8e42c806a24865838f344ac0af7da7c25a28ebd
jeasoncues <73402014+jeasoncues@users.noreply.github.com>
2021-06-22 12:22:26 -0500

    email

---
+++ js/email.js
@@ -0,0 +1,30 @@
+
+
+ const enviar  =  document.querySelector('#enviar');
+ const formulario  = document.querySelector('#formulario');
+
+ enviar.addEventListener('click',function(e){
+     e.preventDefault();
+     console.log('diste click');
+     sendEmail();
+ })
+
+
+ function sendEmail(params) {
+    var tempParams = {
+        nombre: document.getElementById("nombre").value,
+        email: document.getElementById("email").value,
+        celular: document.getElementById("celular").value,
+        asunto: document.getElementById("asunto").value,
+        mensaje: document.getElementById("mensaje").value,
+    };
+
+    emailjs.send('service_7f92o5p','template_uskg3gu',tempParams)
+     .then(function(res){
+        swal("¡Estamos para ayudarte!", "Recuerda que protegemos tus datos", "success");
+        formulario.reset();
+     })
+    
+}
+
+
